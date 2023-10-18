44d5314b04aea383bbfbcffee72ac46503fc8d06
Ranjan098 <144373098+Ranjan098@users.noreply.github.com>
2023-10-19 00:51:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,9 +58,9 @@ bot = Client("bot",
              bot_token=os.environ.get("BOT_TOKEN"),
              api_id=int(os.environ.get("API_ID")),
              api_hash=os.environ.get("API_HASH"))
-auth_users = [5335824446,6285241743,5934830127]
+auth_users = [5318243282]
 sudo_users = auth_users
-sudo_groups = [-949755385]
+sudo_groups = [-1001699711548]
 
 shell_usage = f"**USAGE:** Executes terminal commands directly via bot.\n\n<pre>/shell pip install requests</pre>"
 def one(user_id):
