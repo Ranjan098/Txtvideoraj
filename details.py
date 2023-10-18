8cd2174779c1ade511c893ed53f7680f9db2430e
Ranjan098 <144373098+Ranjan098@users.noreply.github.com>
2023-10-19 00:39:37 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 22210647
-api_hash = "ce0ad2f77f465ab616635a5d7204bb20"
-bot_token = "6521788733:AAHMgXv3UeTEktSCvg9DOSzMDxh98dIycFs"
+api_id = 12606917
+api_hash = f25113b8c17dca6fa7abda53a86bd4f7
+bot_token = 6593486018:AAG-m2cKlwZAoUcMzQCHEZtZ3o5oDGyCr2o
 
